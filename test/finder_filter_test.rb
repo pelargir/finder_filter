287ee746bbc0180f01b2d0e6fe0a0ba96c212d17
Matthew Bass <matt@Anacreon.(none)>
2008-08-09 12:03:36 -0500

    cleaned up tests

--- test/finder_filter_test.rb
+++ test/finder_filter_test.rb
@@ -1,45 +1,55 @@
 require File.expand_path(File.dirname(__FILE__) + '/test_helper')
 
-class TestController < ActionController::Base; end
-class TestsController < ActionController::Base; end
+class TestController < ActionController::Base; extend FinderFilter; end
+class TestsController < ActionController::Base; extend FinderFilter; end
 
 describe FinderFilter do
-  it "should assign before filter and define method when model specified" do
-    controller = TestController
-    controller.extend FinderFilter
-    controller.expects(:before_filter).with("find_foo", {})
-    controller.expects(:define_method).with("find_foo")
-    controller.finder_filter :foo
-  end
+  before { @controller = TestController }
   
-  it "should assign before filter and define method when model not specified" do
-    controller = TestController
-    controller.extend FinderFilter
-    controller.expects(:before_filter).with("find_test", {})
-    controller.expects(:define_method).with("find_test")
-    controller.finder_filter
+  it "should assign before filter when model specified" do
+    @controller.expects(:before_filter).with("find_foo", {})
+    @controller.finder_filter :foo
   end
-  
-  it "should assign before filter and define method when model not specified and controller name is plural" do
+
+  it "should assign before filter when model not specified" do
+    @controller.expects(:before_filter).with("find_test", {})
+    @controller.finder_filter
+  end
+
+  it "should assign before filter when model not specified and controller name is plural" do
     controller = TestsController
-    controller.extend FinderFilter
     controller.expects(:before_filter).with("find_test", {})
+    controller.finder_filter
+  end
+
+  it "should assign before filter when options are specified" do
+    @controller.expects(:before_filter).with("find_test", {})
+    @controller.finder_filter :by => :permalink, :param => :name
+  end
+
+  it "should assign before filter with options" do
+    @controller.expects(:before_filter).with("find_test", :only => [:show, :edit])
+    @controller.finder_filter :only => [:show, :edit]
+  end
+  
+  it "should define method when model specified" do
+    @controller.expects(:define_method).with("find_foo")
+    @controller.finder_filter :foo
+  end
+
+  it "should define method when model not specified" do
+    @controller.expects(:define_method).with("find_test")
+    @controller.finder_filter
+  end
+
+  it "should define method when model not specified and controller name is plural" do
+    controller = TestsController
     controller.expects(:define_method).with("find_test")
     controller.finder_filter
   end
-  
-  it "should assign before filter and define method when options are specified" do
-    controller = TestController
-    controller.extend FinderFilter
-    controller.expects(:before_filter).with("find_test", {})
-    controller.expects(:define_method).with("find_test")
-    controller.finder_filter :by => :permalink, :param => :name
-  end
-  
-  it "should assign before filter with options" do
-    controller = TestController
-    controller.extend FinderFilter
-    controller.expects(:before_filter).with("find_test", :only => [:show, :edit])
-    controller.finder_filter :only => [:show, :edit]
+
+  it "should define method when options are specified" do
+    @controller.expects(:define_method).with("find_test")
+    @controller.finder_filter :by => :permalink, :param => :name
   end
 end
