1bca5ad7a0910eeb21e5fd634acf7f5080cb0616
Matthew Bass <matt@anacreon.local>
2008-08-15 07:29:01 -0400

    added support for nested resources

--- lib/finder_filter.rb
+++ lib/finder_filter.rb
@@ -2,7 +2,7 @@ module FinderFilter
   def finder_filter(*args)
     options = args.extract_options!
     name = args.empty? ? controller_name.singularize : args.first
-    by = options.delete(:by)
+    by, nested = options.delete(:by), options.delete(:nested)
     param = options.delete(:param) || :id
     prepend = options.delete(:prepend) || false
     
@@ -11,7 +11,16 @@ module FinderFilter
     
     define_method "find_#{name}" do
       klass = name.to_s.classify.constantize
-      item = by ? klass.send("find_by_#{by}", params[param]) : klass.find(params[param])
+      if nested
+        nested_klass = nested.to_s.classify.constantize
+        nested_param = "#{nested}_id".intern
+                
+        nested_item = by ? nested_klass.send("find_by_#{by}", params[nested_param]) : nested_klass.find(params[nested_param])
+        item = by ? nested_item.send(name.to_s.pluralize).send("find_by_#{by}", params[param]) : nested_item.send(name.to_s.pluralize).find(params[param])
+        instance_variable_set("@#{nested}", nested_item)
+      else
+        item = by ? klass.send("find_by_#{by}", params[param]) : klass.find(params[param])
+      end
       instance_variable_set("@#{name}", item)
     end
   end
