b63df2d883be961d13d364ad8d49c7b8ed60ecbd
Mike Gunderloy <MikeG1@larkfarm.com>
2008-09-06 19:07:00 -0500

    Fix tests to work from rake.

--- test/finder_filter_finding_test.rb
+++ test/finder_filter_finding_test.rb
@@ -7,35 +7,35 @@ class Tome < ActiveRecord::Base; def param_column; "foo"; end; end
 
 describe FinderFilter do
   before do
-     @controller = BooksController.new
+     @books_controller = BooksController.new
   end
   
   it "should call find on the model" do
      BooksController.finder_filter
-     @controller.params = {:id => 7}
+     @books_controller.params = {:id => 7}
      Book.expects(:find).with(7)
-     @controller.find_book
+     @books_controller.find_book
   end
 
   it "should call find on a column when specified" do
      BooksController.finder_filter :by => :name
-     @controller.params = {:id => "foo"}
+     @books_controller.params = {:id => "foo"}
      Book.expects(:find_by_name).with("foo")
-     @controller.find_book
+     @books_controller.find_book
   end
 
   it "should call find on a different model when specified" do
      BooksController.finder_filter :novel
-     @controller.params = {:id => 7}
+     @books_controller.params = {:id => 7}
      Novel.expects(:find).with(7)
-     @controller.find_novel
+     @books_controller.find_novel
   end
 
   it "should call from_param when param_column is defined" do
     BooksController.finder_filter :tome
-    @controller.params = {:id => 7}
+    @books_controller.params = {:id => 7}
     Tome.expects(:from_param).with(7)
-    @controller.find_tome
+    @books_controller.find_tome
   end
 
 end
